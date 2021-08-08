d0c981ebf69fca5ed94254828a7100b0c669287c
Rudy Goldfberg <notspamclassified500@gmail.com>
2021-08-08 05:56:40 -0500

    Lithium world gen and attempted Basalite but I fucking hate world gen i fucking hate it so much where the fuck is the underworld what fucking tiles are the underworld? How do I constantly check for downedGolemBoss within the world file? Let me just have it gen through the whole world and just replace ash blocks so that its only the underworld, JUST KIDDING UR IF LOOP ISNTWORKING. SO HOW DO I DO IT IT ISNT CHECKING AFTER BOSS IS DEAD. I AM FUCKING DONE WITH THIS PLUS I DIDN'T EVEN DO NETCODE CAUSE I COULD CARE FUCKING LESS

---
+++ NPCs/NPC.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.World.Generation;
+using Pandemonium;
+
+
+namespace Pandemonium.NPCs
+{
+    public class NPC : GlobalNPC
+    {
+    }
+}
---
+++ World/PandemoniumWorld.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.World.Generation;
+
+namespace Pandemonium
+{
+    public class PandemoniumWorld : ModWorld
+    {
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+        {
+            int shiniesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Shinies"));
+            if(shiniesIndex != -1)
+            {
+                tasks.Insert(shiniesIndex + 1, new PassLegacy("Lithium Deposits", GenerateLithiumOre));
+            }
+            if(NPC.downedGolemBoss = true)
+            {
+                tasks.Insert(shiniesIndex + 1, new PassLegacy("Lithium Deposits", GenerateBasalite));
+            }
+        }
+
+        private void GenerateLithiumOre(GenerationProgress progress)
+        {
+            progress.Message = "Generating Lithium Deposits";
+            for(var i = 0; i < (Main.maxTilesY * Main.maxTilesX) * 6E-05; i++)
+            {
+                int x = WorldGen.genRand.Next(200, Main.maxTilesX - 200);
+                int y = WorldGen.genRand.Next((int)WorldGen.worldSurfaceLow, Main.maxTilesY - 500);
+
+                WorldGen.TileRunner(x, y, WorldGen.genRand.Next(6, 12), WorldGen.genRand.Next(3, 6), ModContent.TileType<Items.Tiles.LithiumOre>());
+            }
+        }
+        private void GenerateBasalite(GenerationProgress progress)
+        {
+            if(NPC.downedGolemBoss && Main.hardMode)
+            {
+                int x = WorldGen.genRand.Next(19200, 17000);
+                int y = WorldGen.genRand.Next(19200, 17000);
+
+                WorldGen.TileRunner(x, y, WorldGen.genRand.Next(3, 5), WorldGen.genRand.Next(3, 6), ModContent.TileType<Items.Tiles.BasaltOre>());
+            }
+        }
+        public override void PostWorldGen()
+        {
+            if (NPC.downedGolemBoss == true)
+            {
+                int x = WorldGen.genRand.Next(Main.maxTilesX, Main.maxTilesX - Main.maxTilesX);
+                int y = WorldGen.genRand.Next(Main.maxTilesY, Main.maxTilesX - Main.maxTilesX);
+                Tile t = Framing.GetTileSafely(x, y);
+                if (t.active())
+                {
+                    if(t.type == TileID.Ash)
+                    {
+                        WorldGen.OreRunner(x, y, WorldGen.genRand.Next(3, 5), WorldGen.genRand.Next(3, 6), (ushort)ModContent.TileType<Items.Tiles.BasaltOre>());
+                    }
+                }
+            }
+        }
+
+    }
+}
